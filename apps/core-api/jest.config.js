d852cb44daa39531c650a04f45c0a2ae3a500bd4
yosiwizman <135239829+yosiwizman@users.noreply.github.com>
2025-12-10 08:36:52 -0500

    fix: add ts-jest config for core-api and re-enable CI tests

---
+++ apps/core-api/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/*.test.ts', '**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts', '!src/**/__tests__/**'],
+  moduleNameMapper: {
+    '^@greenenergy/(.*)$': '<rootDir>/../../packages/$1/src',
+  },
+};
